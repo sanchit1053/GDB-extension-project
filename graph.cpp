fc608fa64d155d0a5a2c8e6e76692cef2488417a
Arya Amin <88593404+aryaamin@users.noreply.github.com>
2021-10-20 13:09:55 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -16,17 +16,17 @@ class asdf{
         vector<Node> qwer;
         vector<pair<Node,Node>> zxcv; 
 };
-
 int main(){
     Node *n[2] = {new Node(1), new Node(2)};
     asdf *p = new asdf();
     p->qwer.push_back(*n[0]);
     p->qwer.push_back(*n[1]);
+    p->zxcv.push_back(make_pair(*n[0],*n[1]));
     for(int i = 3; i< 10; i++){
         Node *x = new Node(i);
         p->qwer.push_back(*x);
         p->zxcv.push_back(make_pair(*n[rand()%2],*x));
     }
-    p->zxcv.push_back(make_pair(*n[0],*n[1]));
+   
     return 0; 
 }
