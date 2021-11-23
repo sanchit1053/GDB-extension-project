d925952ab7dac46ace514a7b2fcc65e379e1b54c
aryan-th <200040150@iitb.ac.in>
2021-11-23 16:09:57 +0530

    Uploading on home page

--- application.py
+++ application.py
@@ -15,32 +15,7 @@ def index():
     if request.method == 'GET':
         return render_template("index.html", data = (data))
     if request.method == 'POST':
-        command = request.form['cmd']
-        response = gdb.write(command)
-
-
-    
-        return jsonify({'status': 200 , 'data' : data, 'response':response})
-        return render_template("index.html", data = (data), response = response)
-
-app.config["FILE_UPLOADS"] = "/mnt/c/Users/capts/Desktop/gdb_extension/gdb_project/codefiles"
-app.config["ALLOWED_FILE_EXTENSIONS"] = "CPP"   
-
-def allowed_file(filename):
-    if not "." in filename:
-        return False
-    ext = filename.rsplit(".", 1)[1]
-
-    if ext.upper() in app.config["ALLOWED_FILE_EXTENSIONS"]:
-         return True
-    else:
-        return False
-
-@app.route("/upload-file", methods = ['POST' , 'GET'])
-def upload_file():
-        if request.method == 'POST':
-
-            if request.files:
+        if request.files:
 
                 file = request.files["inpFile"]
 
@@ -59,10 +34,72 @@ def upload_file():
                 file.save(os.path.join(app.config["FILE_UPLOADS"], file.filename))
 
                 print('File saved')
+                print(file.filename)
 
-                return redirect(request.url)
+                file_path = "/mnt/c/Users/capts/Desktop/gdb_extension/gdb_project/codefiles/" + file.filename
 
-        return render_template("upload_file.html")
+                print(file_path)
+
+                with open(file_path,'r') as f:
+                    details = f.read().replace('\n','<br>')
+
+                return render_template("index.html",details = details,data = (data))
+        else:
+            command = request.form['cmd']
+            response = gdb.write(command)
+
+            return jsonify({'status': 200 , 'data' : data, 'response':response})
+        #return render_template("index.html", data = (data), response = response)
+
+app.config["FILE_UPLOADS"] = "/mnt/c/Users/capts/Desktop/gdb_extension/gdb_project/codefiles"
+app.config["ALLOWED_FILE_EXTENSIONS"] = "CPP"
+
+def allowed_file(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_FILE_EXTENSIONS"]:
+         return True
+    else:
+        return False
+
+#{@app.route("/upload-file", methods = ['POST' , 'GET'])
+#def upload_file():
+#        if request.method == 'POST':
+#
+#            if request.files:
+
+#                file = request.files["inpFile"]
+
+#                if file.filename == "":
+#                    print("Image must have a filename")
+#                    return redirect(request.url)
+
+#                if not allowed_file(file.filename):
+#                    print("This file extension is not supported")
+#                    return redirect(request.url)
+
+#                else:
+#                    filename = secure_filename(file.filename)
+                  
+
+#                file.save(os.path.join(app.config["FILE_UPLOADS"], file.filename))
+
+#                print('File saved')
+#                print(file.filename)
+
+#                file_path = "/mnt/c/Users/capts/Desktop/gdb_extension/gdb_project/codefiles/" + file.filename
+
+#                print(file_path)
+
+#                with open(file_path,'r') as f:
+#                    details = f.read().replace('\n','<br>')
+#                    print(details)
+
+#                return render_template("file_content.html", details=details)
+
+#        return render_template("upload_file.html")
 
 
 
