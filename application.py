faf441116f181a33b3f4c2358ad9ccff38d05d95
Arya Amin <88593404+aryaamin@users.noreply.github.com>
2021-11-28 09:25:52 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,25 +1,86 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
+
 import json
 from pygdbmi.gdbcontroller import GdbController
+
+#
+import os
+from werkzeug.utils import secure_filename
+#
+
 app = Flask(__name__)
 
-votes = 0
+#
+# dir = os.getcwd()
+# parent_dir = os.path.dirname(dir)
+# app.config["FILE_UPLOADS"] = parent_dir
+# app.config["ALLOWED_FILE_EXTENSIONS"] = "CPP"  
+
+# def allowed_file(filename):
+#     if not "." in filename:
+#         return False
+#     ext = filename.rsplit(".", 1)[1]
+
+#     if ext.upper() in app.config["ALLOWED_FILE_EXTENSIONS"]:
+#          return True
+#     else:
+#         return False
+
+# #
+
+
+
 
 @app.route("/", methods = ['POST' , 'GET'])
 def index():
+    # reading the sampple.json file which has the data about the graph
     with open('sample.json', 'r') as file:
         data = json.load(file)
+
+    # if first time return the original data
     if request.method == 'GET':
         return render_template("index.html", data = (data))
+
+    # if command made on page then process the commandand send back the response
     if request.method == 'POST':
+        ###################
+        # if request.files:
+        #         file = request.files["inpFile"]
+        #         if file.filename == "":
+        #             print("Image must have a filename")
+        #             return redirect(request.url)
+        #         if not allowed_file(file.filename):
+        #             print("This file extension is not supported")
+        #             return redirect(request.url)
+        #         else:
+        #             filename = secure_filename(file.filename)
+        #         file.save(os.path.join(app.config["FILE_UPLOADS"], file.filename))
+        #         print('File saved')
+        #         print(file.filename)
+        #         file_path =os.path.join(parent_dir,file.filename)
+        #         print(file_path)
+        #         with open(file_path,'r') as f:
+        #             details = f.read().replace('\n','<br>')
+        #         return render_template("index.html",details = details,data = (data))
+        # else:
+########################
+        # read the command
         command = request.form['cmd']
-        response = gdb.write(command)
+        # process the command
+        response = gdb.write(command)        
+        # return the json of the data
+        return jsonify({'status': 200 , 'data' : data, 'response':response})
         return render_template("index.html", data = (data), response = response)
 
 def apprun():   
     app.run(debug = True)
 
 if __name__ == "__main__":
+    # initializing a gdb controller to run commands
     gdb = GdbController();
+    # adding the pyt file to the gdb
     gdb.write("source pyt.py")
     app.run(debug=True)
+    # closing the gdb controller
+    gdb.write("q")
